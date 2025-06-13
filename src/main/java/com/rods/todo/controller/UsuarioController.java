bd35263ef9bd56565536288b42c7b54fb8d50090
rods <rods.desenvolvedor@gmail.com>
2025-06-13 14:38:17 -0400

    feat: add função e endpoint para cadastrar usuario

--- src/main/java/com/rods/todo/controller/UsuarioController.java
+++ src/main/java/com/rods/todo/controller/UsuarioController.java
@@ -2,9 +2,14 @@ package com.rods.todo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rods.todo.dtos.usuario.UsuarioRequestDto;
+import com.rods.todo.dtos.usuario.UsuarioResponseDto;
+import com.rods.todo.entity.Usuario;
 import com.rods.todo.service.UsuarioService;
 
 @RestController
@@ -22,6 +27,14 @@ public class UsuarioController {
     public ResponseEntity<String> teste()
     {
         return ResponseEntity.ok().body("ok: teste bem sucedido.");
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UsuarioResponseDto> cadastrarUsuario(@RequestBody UsuarioRequestDto usuarioRequestDto)
+    {
+        UsuarioResponseDto usuarioResponseDto = usuarioService.cadastrarUsuario(usuarioRequestDto);
+
+        return ResponseEntity.ok().body(usuarioResponseDto);
     }
     
 }
--- src/main/java/com/rods/todo/service/UsuarioService.java
+++ src/main/java/com/rods/todo/service/UsuarioService.java
@@ -1,7 +1,15 @@
 package com.rods.todo.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.rods.todo.dtos.usuario.UsuarioRequestDto;
+import com.rods.todo.dtos.usuario.UsuarioResponseDto;
+import com.rods.todo.entity.Habito;
+import com.rods.todo.entity.Usuario;
 import com.rods.todo.repository.UsuarioRepository;
 
 @Service
@@ -13,5 +21,17 @@ public class UsuarioService {
     {
         this.usuarioRepository = usuarioRepository;
     }
+
+    public UsuarioResponseDto cadastrarUsuario(UsuarioRequestDto usuarioRequestDto)
+    {
+        List<Habito> habitos = new ArrayList<>();
+        Usuario usuario = new Usuario(usuarioRequestDto.username(),
+        usuarioRequestDto.email(),
+        usuarioRequestDto.senha(),
+        LocalDate.now(), habitos);
+
+        return new UsuarioResponseDto(usuario);
+    }
+
     
 }
