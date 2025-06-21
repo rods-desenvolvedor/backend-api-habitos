0574dc3dd8d8fe3b94f54959955a5591dad7537c
rods <rods.desenvolvedor@gmail.com>
2025-06-21 11:21:28 -0400

    feat: metodo para buscar user details pelo email

--- src/main/java/com/rods/todo/repository/UsuarioRepository.java
+++ src/main/java/com/rods/todo/repository/UsuarioRepository.java
@@ -1,11 +1,15 @@
 package com.rods.todo.repository;
 
+
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.rods.todo.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
+
+    public UserDetails getByEmail(String email);
     
 }
