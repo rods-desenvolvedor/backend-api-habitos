a5b6d43aa7d3f04a0e061adde4239678e6ad5d16
rods <rods.desenvolvedor@gmail.com>
2025-06-24 20:04:36 -0400

    feat: add validações nos dtos de login e cadastro de usuario

--- src/main/java/com/rods/todo/dtos/auth/AuthRequestDto.java
+++ src/main/java/com/rods/todo/dtos/auth/AuthRequestDto.java
@@ -1,7 +1,6 @@
 package com.rods.todo.dtos.auth;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
--- src/main/java/com/rods/todo/dtos/auth/RegisterDto.java
+++ src/main/java/com/rods/todo/dtos/auth/RegisterDto.java
@@ -1,9 +1,21 @@
 package com.rods.todo.dtos.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterDto(
 
+    @NotBlank
+    @Size(min = 3, message = "username deve ter pelo menos 3 caracteres")
     String username,
+
+    @Email
+    @NotBlank
     String email,
+
+    @NotBlank
+    @Size(min = 6, message = "senha deve ter pelo menos 6 caracteres")
     String senha
 ) {
     
